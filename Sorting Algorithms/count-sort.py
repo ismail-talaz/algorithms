6ec593ed5afbc61b19190a72bc578e808588cdd9
ismail-talaz <sml0_tlz0@hotmail.com>
2023-07-28 20:09:06 +0300

    Update count-sort.py

--- Sorting Algorithms/count-sort.py
+++ Sorting Algorithms/count-sort.py
@@ -15,3 +15,9 @@ def count_sort(arr):
         counts[k-lowest] -= 1
 
     return result
+
+
+if __name__ == '__main__':
+    arr = [0,0,-2,-90,31,50,50,3,7,77,7,26,5,9]
+    count_sort(arr)
+    print("Sorted Array:",arr)
