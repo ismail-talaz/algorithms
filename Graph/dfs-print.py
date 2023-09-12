64229c12b0995f067eb81b97d901143cf513e26f
ismail-talaz <sml0_tlz0@hotmail.com>
2023-09-12 21:12:49 +0300

    Update dfs-print.py

--- Graph/dfs-print.py
+++ Graph/dfs-print.py
@@ -1,28 +1,34 @@
 from stack import Stack
 
 def iterativeDfsPrint(graph,src):
-
+    visited=set()
     stack=Stack()
     stack.push(src)
     while stack.isEmpty()==False:
         current=stack.pop()
+        if current in visited:continue
+        else:visited.add(current)
         print(current)
         if current not in graph:continue
         for neighbour in graph[current]:
             stack.push(neighbour) 
 
-def recursiveDfsPrint(graph,src):
+def recursiveDfsPrint(graph,src,visited=set()):
+
+    if src in visited:return
     print(src)
+    visited.add(src)
+
     if src not in graph:return
     for neighbour in graph[src]:
-        recursiveDfsPrint(graph,neighbour)
+        recursiveDfsPrint(graph,neighbour,visited)
 
 graph={
     "a":["b","c"],
     "b":["d","e"],
     "c":["f"],
     "f":["z"],
-    "e":["z"]
+    "e":["a"]
 }
 
 iterativeDfsPrint(graph,"a")
