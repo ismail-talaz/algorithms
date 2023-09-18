cad2ad050d3d39f118cf3855c37f8a2c495ce7ef
ismail-talaz <sml0_tlz0@hotmail.com>
2023-09-18 09:52:44 +0300

    Update quick-sort.py

--- Sorting Algorithms/quick-sort.py
+++ Sorting Algorithms/quick-sort.py
@@ -15,7 +15,23 @@ def partition(arr,p,r):                                # Best-case Running Time 
             arr[i],arr[j]=arr[j],arr[i]
     arr[i+1],arr[r]=arr[r],arr[i+1]
     return i+1
-
+    
+def hoarePartition(arr,p,r):
+    pivot=arr[p]
+    i,j=p-1,r+1
+    while (True):
+        while True:
+            i+=1
+            if arr[i]>=pivot:
+                break
+        while True:
+            j-=1
+            if arr[j]<=pivot:
+                break
+        if i<j:
+            arr[i],arr[j]=arr[j],arr[i]
+        else:
+            return j
  
 if __name__ == '__main__':                # In place sorting
     array=[2,4,345,3,56,3444,99,7,68]    
